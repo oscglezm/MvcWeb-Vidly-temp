2324955fc8f338dd03a0095f3167a0c133f7cc80
oscglezm <oscglezm@gmail.com>
2018-06-25 19:22:23 -0400

    v1.2.3

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using MovieWebApp.Models;
+using MovieWebApp.ViewModels;
 
 namespace MovieWebApp.Controllers
 {
---
+++ ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+ 
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieWebApp.ViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+    }
+}
